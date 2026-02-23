2468fd2c1d6050ee292bca2157e39df75cd2e1cf
Berit Haugen <movies.tv.and.music@gmail.com>
2026-02-22 18:26:26 -0600

    first_objects.js

---
+++ Conference_Registration/first_objects.js
@@ -0,0 +1,6 @@
+// Create an object - contains properties
+// Properties are name: value pairs
+let user = {username: "Berit", password: "password123"}
+
+console.log(user.username) // Berit
+console.log(user["username"]) // Berit
